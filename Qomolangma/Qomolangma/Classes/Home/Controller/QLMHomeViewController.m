33714ee8cc739eba2ecffce64e792fa05a060ed3
张晨晖 <364338801@qq.com>
2017-03-16 11:04:54 +0800

    我勒个去~~~

--- Qomolangma/Qomolangma/Classes/Home/Controller/QLMHomeViewController.m
+++ Qomolangma/Qomolangma/Classes/Home/Controller/QLMHomeViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "QLMHomeViewController.h"
+#import "QLMNetworkTools.h"
 
 @interface QLMHomeViewController ()
 
@@ -29,35 +30,63 @@
     self.view.backgroundColor = [UIColor whiteColor];
     // Do any additional setup after loading the view.
     
-//    [self loadData];
+    [self loadData];
     
 }
 
 #pragma 2 - 加载首页数据
 - (void)loadData {
     
-    //加载数据
-    NSURL *url = [NSURL URLWithString:@"http://iosapi.itcast.cn:8080/app/home/getHomeData"];
-    NSData *data = [NSData dataWithContentsOfURL:url];
-    
-    if (data == nil) {//网络有问题
-        //弹出错误信息    /////////这里需要用到弹出
-        NSLog(@"错误");
+	NSString *URLStr = @"http://121.42.205.189:8080/app/home/getHomeData";
+    [[QLMNetworkTools sharedTools] requestWithType:GET andUrlStr:URLStr andParams:nil andSuccess:^(id responseObject) {
+        NSDictionary *responseData = [NSJSONSerialization JSONObjectWithData:responseObject options:0 error:nil];
+        self.homeDataDic = [responseData objectForKey:@"data"];
+        NSLog(@"homeDataDic:%@",_homeDataDic);
+    } andFailture:^(NSError *error) {
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:nil message:@"没有网络" preferredStyle:UIAlertControllerStyleAlert];
         [self presentViewController:alert animated:YES completion:^{
             //完成取消
-            [NSThread sleepForTimeInterval:1.0f];
+            [NSThread sleepForTimeInterval:.5f];
             [alert dismissViewControllerAnimated:YES completion:nil];
+            NSLog(@"error:%@",error);
         }];
-        
-    } else {
-        NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        NSDictionary *homeDataDic = [dic objectForKey:@"data"];
-        self.homeDataDic = homeDataDic;
-//        NSLog(@"homeDataDic:%@",homeDataDic);
-    }
+    }];
+
+
+//    //加载数据
+//    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+//    
+//    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", @"text/plain", nil];
+//    
+//    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+//    
+//    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+//    
+//    NSString *URLStr = @"http://121.42.205.189:8080/app/home/getHomeData";
+//    
+//    [manager GET:URLStr parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//        
+////        NSString *html = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+////        NSLog(@"%@",html);
+////        NSLog(@"%@",responseObject);
+//        NSDictionary *responseData = [NSJSONSerialization JSONObjectWithData:responseObject options:0 error:nil];
+//        NSLog(@"data:%@",responseData);
+//        //        NSLog(@"%@", responseObject);
+//        self.homeDataDic = [responseData objectForKey:@"data"];
+//        NSLog(@"homeDataDic:%@",_homeDataDic);
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+////        NSLog(@"%@",error);
+//        UIAlertController *alert = [UIAlertController alertControllerWithTitle:nil message:@"没有网络" preferredStyle:UIAlertControllerStyleAlert];
+//        [self presentViewController:alert animated:YES completion:^{
+//            //完成取消
+//            [NSThread sleepForTimeInterval:.5f];
+//            [alert dismissViewControllerAnimated:YES completion:nil];
+//        }];
+//    }];
     
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
