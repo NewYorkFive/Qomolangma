9a3bb3fa2c291780fa97c48374157feb1d2cae0d
NowOrNever <tobeabetterman_dl@163.com>
2017-03-16 09:24:04 +0800

    添加tool公共类，并添加到pch中

---
+++ Qomolangma/Qomolangma/Classes/Common/Tools/QLMNetworkTools.m
@@ -0,0 +1,47 @@
+//
+//  QLMNetworkTools.m
+//  Qomolangma
+//
+//  Created by NowOrNever on 16/03/2017.
+//  Copyright © 2017 Focus. All rights reserved.
+//
+
+#import "QLMNetworkTools.h"
+
+@implementation QLMNetworkTools
+
++ (instancetype)sharedTools {
+    static QLMNetworkTools *tools;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        tools = [[QLMNetworkTools alloc] initWithBaseURL:[NSURL URLWithString:@"http://c.m.163.com/nc/article/list/"]];
+    });
+    return tools;
+}
+
+
+/**
+ 通用的请求方式
+ 
+ @param requestType 请求类型
+ @param urlStr 请求地址
+ @param parameters 请求参数
+ @param successBlock 成功的回调
+ @param failureBlock 失败的回调
+ */
+- (void)requestWithType: (RequestType)requestType andUrlStr: (NSString *)urlStr andParams: (id)parameters andSuccess: (void (^)(id responseObject))successBlock andFailture: (void (^)(NSError *error))failureBlock{
+    if (requestType == GET) {
+        [self GET:urlStr parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            successBlock(responseObject);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            failureBlock(error);
+        }];
+    } else {
+        [self POST:urlStr parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            successBlock(responseObject);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            failureBlock(error);
+        }];
+    }
+}
+@end
