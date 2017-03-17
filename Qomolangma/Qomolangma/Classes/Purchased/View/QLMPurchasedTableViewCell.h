9b69c4894d8a86581bb139e00b36fff635b3dc80
Will <adrianwhp@163.com>
2017-03-17 21:03:40 +0800

    已购内容界面设置

---
+++ Qomolangma/Qomolangma/Classes/Purchased/View/QLMPurchasedTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  QLMPurchasedTableViewCell.h
+//  Qomolangma
+//
+//  Created by 王惠平 on 2017/3/17.
+//  Copyright © 2017年 Focus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QLMPurchasedTableViewCell : UITableViewCell
+
+@end
