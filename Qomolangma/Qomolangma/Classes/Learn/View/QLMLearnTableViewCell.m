bac46c3fa28aa579ddfa215d6920b0066de924d1
刘康 <1187176908@qq.com>
2017-03-17 11:31:05 +0800

    二级界面tableViewUI搭建

--- Qomolangma/Qomolangma/Classes/Learn/View/QLMLearnTableViewCell.m
+++ Qomolangma/Qomolangma/Classes/Learn/View/QLMLearnTableViewCell.m
@@ -11,14 +11,93 @@
 @implementation QLMLearnTableViewCell
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    [self setupUI];
-    return [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
+    if (self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier]) {
+        [self setupUI];
+    }
+    return self;
 }
 
 //自定义tableCell,添加数据
 - (void)setupUI {
     self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    //添加控件
+    //image
+    UIImageView *learnBookImageView = [[UIImageView alloc] init];
+    learnBookImageView.image = [UIImage imageNamed:@"placeholderImage"];
+    //title
+    UILabel *learnBookTitleLabel = [[UILabel alloc] init];
+    learnBookTitleLabel.text = @"读古希腊神话学营销";
+    //reader
+    UILabel *learnBookreadersLabel = [[UILabel alloc] init];
+    learnBookreadersLabel.text = @"1人订阅";
+    learnBookreadersLabel.font = [UIFont systemFontOfSize:12];
+    learnBookreadersLabel.textColor = [UIColor lightGrayColor];
+    //text
+    UILabel *learnBookTextLabel = [[UILabel alloc] init];
+    learnBookTextLabel.text = @"杨烁今日称小乐老师2010年中国年度十大杨烁今日称小乐老师2010年中国年度十大";
+    learnBookTextLabel.font = [UIFont systemFontOfSize:12];
+    learnBookTextLabel.textColor = [UIColor grayColor];
+    //introduce
+    UILabel *learnBookIntroduceLabel = [[UILabel alloc] init];
+    learnBookIntroduceLabel.text = @"从古希腊神话中系统地学习营销之道";
+    learnBookIntroduceLabel.font = [UIFont systemFontOfSize:12];
+    learnBookIntroduceLabel.textColor = [UIColor lightGrayColor];
+    //price
+    UILabel *learnBookPriceLabel = [[UILabel alloc] init];
+    learnBookPriceLabel.text = @"¥39/月";
+    learnBookPriceLabel.font = [UIFont systemFontOfSize:12];
+    learnBookPriceLabel.textColor = [UIColor brownColor];
+    
+    [self.contentView addSubview:learnBookImageView];
+    [self.contentView addSubview:learnBookTitleLabel];
+    [self.contentView addSubview:learnBookreadersLabel];
+    [self.contentView addSubview:learnBookTextLabel];
+    [self.contentView addSubview:learnBookIntroduceLabel];
+    [self.contentView addSubview:learnBookPriceLabel];
+   
+    [learnBookImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.offset(10);
+        make.bottom.offset(-10);
+        make.width.offset(60);
+    }];
+    [learnBookTitleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self).offset(10);
+        make.left.equalTo(learnBookImageView.mas_right).offset(5);
+    }];
+    [learnBookreadersLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self).offset(10);
+        make.right.equalTo(self).offset(-10);
+    }];
+    [learnBookTextLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(learnBookTitleLabel);
+        make.top.equalTo(learnBookTitleLabel.mas_bottom).offset(5);
+        make.right.equalTo(self).offset(-40);
+    }];
+    [learnBookIntroduceLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(learnBookTitleLabel);
+        make.top.equalTo(learnBookTextLabel.mas_bottom).offset(5);
+        make.right.equalTo(self).offset(-40);
+    }];
+    [learnBookPriceLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(learnBookImageView);
+        make.left.equalTo(learnBookTitleLabel);
+    }];
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
