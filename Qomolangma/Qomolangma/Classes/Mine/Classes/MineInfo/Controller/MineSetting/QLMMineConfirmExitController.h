6549f4c57989d0ce18a6b8bfff78428a04b73a7d
Aioria <aioria1314521@163.com>
2017-03-19 21:27:11 +0800

    设置页面以及子页面

---
+++ Qomolangma/Qomolangma/Classes/Mine/Classes/MineInfo/Controller/MineSetting/QLMMineConfirmExitController.h
@@ -0,0 +1,13 @@
+//
+//  QLMMineConfirmExitController.h
+//  Qomolangma
+//
+//  Created by Aioria on 2017/3/19.
+//  Copyright © 2017年 Focus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QLMMineConfirmExitController : UIViewController
+
+@end
---
+++ Qomolangma/Qomolangma/Classes/Mine/Classes/MineInfo/Views/QLMMineSettingBottomCell.h
@@ -0,0 +1,13 @@
+//
+//  QLMMineSettingBottomCell.h
+//  Qomolangma
+//
+//  Created by Aioria on 2017/3/19.
+//  Copyright © 2017年 Focus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QLMMineSettingBottomCell : UITableViewCell
+
+@end
