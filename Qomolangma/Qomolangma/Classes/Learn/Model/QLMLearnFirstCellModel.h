62f881def6ad1b15978f9a14374c5f7ff0418b5d
刘康 <1187176908@qq.com>
2017-03-20 20:30:03 +0800

    属性

--- Qomolangma/Qomolangma/Classes/Learn/Model/QLMLearnFirstCellModel.h
+++ Qomolangma/Qomolangma/Classes/Learn/Model/QLMLearnFirstCellModel.h
@@ -10,14 +10,23 @@
 
 @interface QLMLearnFirstCellModel : NSObject
 
+///价格单位
 @property(nonatomic,copy) NSString * price_unit;
+///作者简介
 @property (nonatomic, copy) NSString *person_info;
+///人名
 @property (nonatomic, copy) NSString *author;
+///图片地址
 @property (nonatomic, copy) NSString *image_url;
+///价格
 @property (nonatomic, assign) NSInteger price;
+///标题
 @property (nonatomic, copy) NSString *name;
+///id
 @property (nonatomic, assign) NSInteger *modelId;
+///文本介绍
 @property (nonatomic, copy) NSString *content;
+///已读个数
 @property(nonatomic,assign) NSInteger subscribe_count;
 
 @end
